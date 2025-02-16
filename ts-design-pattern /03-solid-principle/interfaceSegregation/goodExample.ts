74eab9b28784d0bd8aaff4c7cb07d5532e9d5e92
TKMhub <111867865+TKMhub@users.noreply.github.com>
2025-02-16 23:59:01 +0900

    interfaceSegregation

--- ts-design-pattern /03-solid-principle/interfaceSegregation/goodExample.ts
+++ ts-design-pattern /03-solid-principle/interfaceSegregation/goodExample.ts
@@ -1,57 +1,57 @@
-export {}
+// export {}
 
-interface Vehicle {
-    name: string;
-    color: string;
-}
+// interface Vehicle {
+//     name: string;
+//     color: string;
+// }
 
-interface Movable {
-    start();
-    stop();
-}
+// interface Movable {
+//     start();
+//     stop();
+// }
 
-interface Flyable {
-    fly();
-}
+// interface Flyable {
+//     fly();
+// }
 
-class Airplane implements Vehicle, Movable, Flyable {
-    constructor(
-        public name: string, 
-        public color: string
-    ) {
+// class Airplane implements Vehicle, Movable, Flyable {
+//     constructor(
+//         public name: string,
+//         public color: string
+//     ) {
 
-    }
-    start() {
-        console.log("start!");
-    }
-    stop() {
-        console.log("stop!");
-    }
-    fly() {
-        console.log("fly!");
-    }
-}
+//     }
+//     start() {
+//         console.log("start!");
+//     }
+//     stop() {
+//         console.log("stop!");
+//     }
+//     fly() {
+//         console.log("fly!");
+//     }
+// }
 
-class Car implements Vehicle, Movable {
-    constructor(
-        public name: string,
-        public color: string
-    ) {}
+// class Car implements Vehicle, Movable {
+//     constructor(
+//         public name: string,
+//         public color: string
+//     ) {}
 
-    start() {
-        console.log("start!");
-    }
-    stop() {
-        console.log("stop!");
-    }
-}
+//     start() {
+//         console.log("start!");
+//     }
+//     stop() {
+//         console.log("stop!");
+//     }
+// }
 
-function run() {
-    const v1 = new Airplane("AirBus", "white");
-    const v2 = new Car("Prius", "black");
+// function run() {
+//     const v1 = new Airplane("AirBus", "white");
+//     const v2 = new Car("Prius", "black");
 
-    v1.fly();
-    v2.start();
-}
+//     v1.fly();
+//     v2.start();
+// }
 
-run();
+// run();
---
+++ ts-design-pattern /03-solid-principle/interfaceSegregation/practiceInterfaceSegregation.ts
@@ -0,0 +1,52 @@
+export {};
+
+interface Vehicle {
+  name: string;
+  color: string;
+}
+
+interface Movable {
+  start();
+  stop();
+}
+
+interface Flyable {
+  fly();
+}
+
+class Airplane implements Vehicle, Movable, Flyable {
+  constructor(public name: string, public color: string) {}
+  start() {
+    console.log("start!");
+  }
+  stop() {
+    console.log("stop!");
+  }
+  fly() {
+    console.log("fly!");
+  }
+}
+
+class Car implements Vehicle, Movable {
+  constructor(public name: string, public color: string) {}
+
+  start() {
+    console.log("start!");
+  }
+  stop() {
+    console.log("stop!");
+  }
+  fly() {
+    throw new Error("車は空を飛べません");
+  }
+}
+
+function run() {
+  const v1 = new Airplane("AirBus", "white");
+  const v2 = new Car("Prius", "black");
+
+  v1.fly();
+  v2.start();
+}
+
+run();

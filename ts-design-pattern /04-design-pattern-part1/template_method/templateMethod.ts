aadba282c17d3ca093da87a6c8fed154771e3070
TKMhub <111867865+TKMhub@users.noreply.github.com>
2025-02-17 23:26:43 +0900

    templatemethod

--- ts-design-pattern /04-design-pattern-part1/template_method/templateMethod.ts
+++ ts-design-pattern /04-design-pattern-part1/template_method/templateMethod.ts
@@ -1,47 +1,47 @@
-export {}
+// export {}
 
-abstract class TestTemplate {
-    test() {
-        this.setup();
-        this.execute();
-        this.teardown();
-    }
+// abstract class TestTemplate {
+//     test() {
+//         this.setup();
+//         this.execute();
+//         this.teardown();
+//     }
 
-    abstract setup();
-    abstract execute();
+//     abstract setup();
+//     abstract execute();
 
-    teardown() {
-        console.log("teardown");
-    }
-}
+//     teardown() {
+//         console.log("teardown");
+//     }
+// }
 
-class ItemServiceTest extends TestTemplate {
-    setup() {
-        console.log("setup: ItemServiceTest");
-    }
+// class ItemServiceTest extends TestTemplate {
+//     setup() {
+//         console.log("setup: ItemServiceTest");
+//     }
 
-    execute() {
-        console.log("execute: ItemServiceTest");
-    }
-}
+//     execute() {
+//         console.log("execute: ItemServiceTest");
+//     }
+// }
 
-class UserServiceTest extends TestTemplate {
-    setup() {
-        console.log("setup: UserServiceTest");
-    }
+// class UserServiceTest extends TestTemplate {
+//     setup() {
+//         console.log("setup: UserServiceTest");
+//     }
 
-    execute() {
-        console.log("execute: UserServiceTest");
-    }
-}
+//     execute() {
+//         console.log("execute: UserServiceTest");
+//     }
+// }
 
-function run() {
-    const itemServiceTest = new ItemServiceTest();
-    const userServiceTest = new UserServiceTest();
+// function run() {
+//     const itemServiceTest = new ItemServiceTest();
+//     const userServiceTest = new UserServiceTest();
 
-    itemServiceTest.test();
-    console.log("");
-    userServiceTest.test();
-}
+//     itemServiceTest.test();
+//     console.log("");
+//     userServiceTest.test();
+// }
 
-run();
+// run();
